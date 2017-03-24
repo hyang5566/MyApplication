dac4ecc0a01e6b9d7c754c5f38b664f114beb93c
huyang <hyang5566@163.com>
2017-03-24 14:18:19 +0800

    第三次上传

--- app/src/main/java/com/crbank/huyang/myapplication/MainActivity.java
+++ app/src/main/java/com/crbank/huyang/myapplication/MainActivity.java
@@ -2,8 +2,11 @@ package com.crbank.huyang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LayoutInflater LayoutInflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
